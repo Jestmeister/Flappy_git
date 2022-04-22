d8824f90061ddfcb9d95d0fe624b7c75adb656a8
Philip <51917644+Philip444@users.noreply.github.com>
2022-04-22 15:31:51 +0200

    Working NN flappy (with example "net")

---
+++ Flappy-Birds-Game/exampleNN.py
@@ -0,0 +1,26 @@
+
+import random
+
+import flappyNN
+
+
+
+def NN(NNInput):
+    print(NNInput)
+
+    probability = 0.1
+    return random.random() < probability
+
+
+
+theGame = flappyNN.Game()
+
+# theGame.Start(displayGameInput, limitFPSInput)
+gameOutput = theGame.Start(True, False)
+
+while(True):
+    #theGame.Update(Jump = input aka network output should be True or False) -> Game state
+    gameOutput = theGame.Update(NN(gameOutput))
+
+    if gameOutput == None:
+        break
--- Flappy-Birds-Game/flappyNN.py
+++ Flappy-Birds-Game/flappyNN.py
@@ -6,214 +6,196 @@ from pygame.locals import *  # Basic pygame imports
 
 
 
-# Global Variables for the game
-FPS = 32
-scr_width = 289
-scr_height = 511
-display_screen_window = pygame.display.set_mode((scr_width, scr_height))
-play_ground = scr_height * 0.8
-game_image = {}
-game_audio_sound = {}
-player = 'images/bird.png'
-bcg_image = 'images/background.png'
-pipe_image = 'images/pipe.png'
+class Game:
+    def __init__(self):
+        self.FPS = 32
+
+        self.scr_width = 289
+        self.scr_height = 511
+        self.display_screen_window = pygame.display.set_mode((self.scr_width, self.scr_height))
+        self.play_ground = self.scr_height * 0.8
+
+        self.game_image = {}
+        self.game_audio_sound = {}
+        self.player = 'images/bird.png'
+        self.bcg_image = 'images/background.png'
+        self.pipe_image = 'images/pipe.png'
+        
+
+        pygame.init()
+        self.time_clock = pygame.time.Clock()
+        pygame.display.set_caption('Flappy Bird Game')
+        self.game_image['numbers'] = (
+            pygame.image.load('images/0.png').convert_alpha(),
+            pygame.image.load('images/1.png').convert_alpha(),
+            pygame.image.load('images/2.png').convert_alpha(),
+            pygame.image.load('images/3.png').convert_alpha(),
+            pygame.image.load('images/4.png').convert_alpha(),
+            pygame.image.load('images/5.png').convert_alpha(),
+            pygame.image.load('images/6.png').convert_alpha(),
+            pygame.image.load('images/7.png').convert_alpha(),
+            pygame.image.load('images/8.png').convert_alpha(),
+            pygame.image.load('images/9.png').convert_alpha(),
+        )
+
+        self.game_image['message'] = pygame.image.load('images/message.png').convert_alpha()
+        self.game_image['base'] = pygame.image.load('images/base.png').convert_alpha()
+        self.game_image['pipe'] = (pygame.transform.rotate(pygame.image.load(self.pipe_image).convert_alpha(), 180),
+                                pygame.image.load(self.pipe_image).convert_alpha()
+                                )
+
+        # Game sounds
+        self.game_audio_sound['die'] = pygame.mixer.Sound('sounds/die.wav')
+        self.game_audio_sound['hit'] = pygame.mixer.Sound('sounds/hit.wav')
+        self.game_audio_sound['point'] = pygame.mixer.Sound('sounds/point.wav')
+        self.game_audio_sound['swoosh'] = pygame.mixer.Sound('sounds/swoosh.wav')
+        self.game_audio_sound['wing'] = pygame.mixer.Sound('sounds/wing.wav')
+
+        self.game_image['background'] = pygame.image.load(self.bcg_image).convert()
+        self.game_image['player'] = pygame.image.load(self.player).convert_alpha()
+
+    
+    def Start(self, displayGameInput, limitFPSInput):
+        self.displayGame = displayGameInput
+        self.limitFPS = limitFPSInput
+
+        self.score = 0
+        self.p_x = int(self.scr_width / 5)
+        self.p_y = int(self.scr_width / 2)
+        self.b_x = 0
 
 
-
-def Start(displayGameInput, limitFPSInput):
-    global displayGame
-    displayGame = displayGameInput
-    global limitFPS
-    limitFPS = limitFPSInput
+        self.n_pip1 = self.get_Random_Pipes()
+        self.n_pip2 = self.get_Random_Pipes()
 
 
-    global score
-    score = 0
-    global p_x
-    p_x = int(scr_width / 5)
-    global p_y
-    p_y = int(scr_width / 2)
-    global b_x
-    b_x = 0
+        self.up_pips = [
+            {'x': self.scr_width + 200, 'y': self.n_pip1[0]['y']},
+            {'x': self.scr_width + 200 + (self.scr_width / 2), 'y': self.n_pip2[0]['y']},
+        ]
+
+        self.low_pips = [
+            {'x': self.scr_width + 200, 'y': self.n_pip1[1]['y']},
+            {'x': self.scr_width + 200 + (self.scr_width / 2), 'y': self.n_pip2[1]['y']},
+        ]
+
+        self.pip_Vx = -4
+
+        self.p_vx = -9
+        self.p_mvx = 10
+        self.p_mvy = -8
+        self.p_accuracy = 1
+
+        self.p_flap_accuracy = -8
+        self.p_flap = False
 
 
-    global n_pip1
-    n_pip1 = get_Random_Pipes()
-    global n_pip2
-    n_pip2 = get_Random_Pipes()
+    def Update(self, jump):
+        if jump:
+            if self.p_y > 0:
+                self.p_vx = self.p_flap_accuracy
+                self.p_flap = True
+                self.game_audio_sound['wing'].play()
 
-
-    global up_pips
-    up_pips = [
-        {'x': scr_width + 200, 'y': n_pip1[0]['y']},
-        {'x': scr_width + 200 + (scr_width / 2), 'y': n_pip2[0]['y']},
-    ]
-
-
-    global low_pips
-    low_pips = [
-        {'x': scr_width + 200, 'y': n_pip1[1]['y']},
-        {'x': scr_width + 200 + (scr_width / 2), 'y': n_pip2[1]['y']},
-    ]
-
-    global pip_Vx
-    pip_Vx = -4
-
-    global p_vx
-    p_vx = -9
-    global p_mvx
-    p_mvx = 10
-    global p_mvy
-    p_mvy = -8
-    global p_accuracy
-    p_accuracy = 1
-
-    global p_flap_accuracy
-    p_flap_accuracy = -8
-    global p_flap
-    p_flap = False
-
-
-def Update(jump):
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-            if p_y > 0:
-                p_vx = p_flap_accuracy
-                p_flap = True
-                game_audio_sound['wing'].play()
-
-        cr_tst = is_Colliding(p_x, p_y, up_pips,
-                              low_pips)
+        cr_tst = self.is_Colliding(self.p_x, self.p_y, self.up_pips,
+                              self.low_pips)
         if cr_tst:
             return
 
 
-        p_middle_positions = p_x + game_image['player'].get_width() / 2
-        for pipe in up_pips:
-            pip_middle_positions = pipe['x'] + game_image['pipe'][0].get_width() / 2
-            if pip_middle_positions <= p_middle_positions < pip_middle_positions + 4:
-                score += 1
-                print(f"Your score is {score}")
-                game_audio_sound['point'].play()
+        self.p_middle_positions = self.p_x + self.game_image['player'].get_width() / 2
+        for pipe in self.up_pips:
+            self.pip_middle_positions = pipe['x'] + self.game_image['pipe'][0].get_width() / 2
+            if self.pip_middle_positions <= self.p_middle_positions < self.pip_middle_positions + 4:
+                self.score += 1
+                print(f"Your score is {self.score}")
+                self.game_audio_sound['point'].play()
 
-        if p_vx < p_mvx and not p_flap:
-            p_vx += p_accuracy
+        if self.p_vx < self.p_mvx and not self.p_flap:
+            self.p_vx += self.p_accuracy
 
-        if p_flap:
-            p_flap = False
-        p_height = game_image['player'].get_height()
-        p_y = p_y + min(p_vx, play_ground - p_y - p_height)
-
-
-        for pip_upper, pip_lower in zip(up_pips, low_pips):
-            pip_upper['x'] += pip_Vx
-            pip_lower['x'] += pip_Vx
+        if self.p_flap:
+            self.p_flap = False
+        self.p_height = self.game_image['player'].get_height()
+        self.p_y = self.p_y + min(self.p_vx, self.play_ground - self.p_y - self.p_height)
 
 
-        if 0 < up_pips[0]['x'] < 5:
-            new_pip = get_Random_Pipes()
-            up_pips.append(new_pip[0])
-            low_pips.append(new_pip[1])
+        for pip_upper, pip_lower in zip(self.up_pips, self.low_pips):
+            pip_upper['x'] += self.pip_Vx
+            pip_lower['x'] += self.pip_Vx
 
 
-        if up_pips[0]['x'] < -game_image['pipe'][0].get_width():
-            up_pips.pop(0)
-            low_pips.pop(0)
+        if 0 < self.up_pips[0]['x'] < 5:
+            new_pip = self.get_Random_Pipes()
+            self.up_pips.append(new_pip[0])
+            self.low_pips.append(new_pip[1])
 
 
-        display_screen_window.blit(game_image['background'], (0, 0))
-        for pip_upper, pip_lower in zip(up_pips, low_pips):
-            display_screen_window.blit(game_image['pipe'][0], (pip_upper['x'], pip_upper['y']))
-            display_screen_window.blit(game_image['pipe'][1], (pip_lower['x'], pip_lower['y']))
+        if self.up_pips[0]['x'] < -self.game_image['pipe'][0].get_width():
+            self.up_pips.pop(0)
+            self.low_pips.pop(0)
 
-        display_screen_window.blit(game_image['base'], (b_x, play_ground))
-        display_screen_window.blit(game_image['player'], (p_x, p_y))
-        d = [int(x) for x in list(str(score))]
+
+        self.display_screen_window.blit(self.game_image['background'], (0, 0))
+        for pip_upper, pip_lower in zip(self.up_pips, self.low_pips):
+            self.display_screen_window.blit(self.game_image['pipe'][0], (pip_upper['x'], pip_upper['y']))
+            self.display_screen_window.blit(self.game_image['pipe'][1], (pip_lower['x'], pip_lower['y']))
+
+        self.display_screen_window.blit(self.game_image['base'], (self.b_x, self.play_ground))
+        self.display_screen_window.blit(self.game_image['player'], (self.p_x, self.p_y))
+        d = [int(x) for x in list(str(self.score))]
         w = 0
         for digit in d:
-            w += game_image['numbers'][digit].get_width()
-        Xoffset = (scr_width - w) / 2
+            w += self.game_image['numbers'][digit].get_width()
+        Xoffset = (self.scr_width - w) / 2
 
         for digit in d:
-            display_screen_window.blit(game_image['numbers'][digit], (Xoffset, scr_height * 0.12))
-            Xoffset += game_image['numbers'][digit].get_width()
-        
-        if displayGame:
+            self.display_screen_window.blit(self.game_image['numbers'][digit], (Xoffset, self.scr_height * 0.12))
+            Xoffset += self.game_image['numbers'][digit].get_width()
+        if self.displayGame:
             pygame.display.update()
+        if self.limitFPS:
+            self.time_clock.tick(self.FPS)
 
-        if limitFPS:
-            time_clock.tick(FPS)
+        #returns game state
+        x_to_pipe = abs(self.p_x - self.up_pips[0]['x'])
+        for pipe in self.up_pips:
+            if pipe['x'] > self.p_x and abs(self.p_x - pipe['x']) < x_to_pipe:
+                x_to_pipe = abs(self.p_x - pipe['x'])
 
-
-def is_Colliding(p_x, p_y, up_pipes, low_pipes):
-    if p_y > play_ground - 25 or p_y < 0:
-        game_audio_sound['hit'].play()
-        return True
-
-    for pipe in up_pipes:
-        pip_h = game_image['pipe'][0].get_height()
-        if (p_y < pip_h + pipe['y'] and abs(p_x - pipe['x']) < game_image['pipe'][0].get_width()):
-            game_audio_sound['hit'].play()
+        return [self.p_y, x_to_pipe]
+        
+    def is_Colliding(self, p_x, p_y, up_pipes, low_pipes):
+        if p_y > self.play_ground - 25 or p_y < 0:
+            self.game_audio_sound['hit'].play()
             return True
 
-    for pipe in low_pipes:
-        if (p_y + game_image['player'].get_height() > pipe['y']) and abs(p_x - pipe['x']) < \
-                game_image['pipe'][0].get_width():
-            game_audio_sound['hit'].play()
-            return True
+        for pipe in up_pipes:
+            pip_h = self.game_image['pipe'][0].get_height()
+            if (p_y < pip_h + pipe['y'] and abs(p_x - pipe['x']) < self.game_image['pipe'][0].get_width()):
+                self.game_audio_sound['hit'].play()
+                return True
 
-    return False
+        for pipe in low_pipes:
+            if (p_y + self.game_image['player'].get_height() > pipe['y']) and abs(p_x - pipe['x']) < \
+                    self.game_image['pipe'][0].get_width():
+                self.game_audio_sound['hit'].play()
+                return True
 
-
-def get_Random_Pipes():
-    """
-    Generate positions of two pipes(one bottom straight and one top rotated ) for blitting on the screen
-    """
-    pip_h = game_image['pipe'][0].get_height()
-    off_s = scr_height / 3
-    yes2 = off_s + random.randrange(0, int(scr_height - game_image['base'].get_height() - 1.2 * off_s))
-    pipeX = scr_width + 10
-    y1 = pip_h - yes2 + off_s
-    pipe = [
-        {'x': pipeX, 'y': -y1},  # upper Pipe
-        {'x': pipeX, 'y': yes2}  # lower Pipe
-    ]
-    return pipe
+        return False
 
 
-if __name__ == "__main__":
-
-    pygame.init()
-    time_clock = pygame.time.Clock()
-    pygame.display.set_caption('Flappy Bird Game')
-    game_image['numbers'] = (
-        pygame.image.load('images/0.png').convert_alpha(),
-        pygame.image.load('images/1.png').convert_alpha(),
-        pygame.image.load('images/2.png').convert_alpha(),
-        pygame.image.load('images/3.png').convert_alpha(),
-        pygame.image.load('images/4.png').convert_alpha(),
-        pygame.image.load('images/5.png').convert_alpha(),
-        pygame.image.load('images/6.png').convert_alpha(),
-        pygame.image.load('images/7.png').convert_alpha(),
-        pygame.image.load('images/8.png').convert_alpha(),
-        pygame.image.load('images/9.png').convert_alpha(),
-    )
-
-    game_image['message'] = pygame.image.load('images/message.png').convert_alpha()
-    game_image['base'] = pygame.image.load('images/base.png').convert_alpha()
-    game_image['pipe'] = (pygame.transform.rotate(pygame.image.load(pipe_image).convert_alpha(), 180),
-                          pygame.image.load(pipe_image).convert_alpha()
-                          )
-
-    # Game sounds
-    game_audio_sound['die'] = pygame.mixer.Sound('sounds/die.wav')
-    game_audio_sound['hit'] = pygame.mixer.Sound('sounds/hit.wav')
-    game_audio_sound['point'] = pygame.mixer.Sound('sounds/point.wav')
-    game_audio_sound['swoosh'] = pygame.mixer.Sound('sounds/swoosh.wav')
-    game_audio_sound['wing'] = pygame.mixer.Sound('sounds/wing.wav')
-
-    game_image['background'] = pygame.image.load(bcg_image).convert()
-    game_image['player'] = pygame.image.load(player).convert_alpha()
+    def get_Random_Pipes(self):
+        """
+        Generate positions of two pipes(one bottom straight and one top rotated ) for blitting on the screen
+        """
+        pip_h = self.game_image['pipe'][0].get_height()
+        off_s = self.scr_height / 3
+        yes2 = off_s + random.randrange(0, int(self.scr_height - self.game_image['base'].get_height() - 1.2 * off_s))
+        pipeX = self.scr_width + 10
+        y1 = pip_h - yes2 + off_s
+        pipe = [
+            {'x': pipeX, 'y': -y1},  # upper Pipe
+            {'x': pipeX, 'y': yes2}  # lower Pipe
+        ]
+        return pipe
