cd4d4ac54ea9c6668cced15c274677a6984953d6
Philip <51917644+Philip444@users.noreply.github.com>
2022-05-04 10:30:01 +0200

    added net

--- Flappy_AxeloPhilip/main.py
+++ Flappy_AxeloPhilip/main.py
@@ -1,286 +1,142 @@
-import random  # For generating random numbers
-import sys
-from turtle import isvisible  # We will use sys.exit to exit the program
-import pygame
-from pygame.locals import *  # Basic pygame imports
-from environment import environment
-from agent import badagent
-# Global Variables for the game
-FPS = 32
-scr_width = 289
-scr_height = 511
-display_screen_window = pygame.display.set_mode((scr_width, scr_height))
-play_ground = scr_height * 0.8
-game_image = {}
-game_audio_sound = {}
-player = 'images/bird.png'
-bcg_image = 'images/background.png'
-pipe_image = 'images/pipe.png'
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Bernoulli
+from torch.autograd import Variable
+from itertools import count
+import matplotlib.pyplot as plt
+import numpy as np
+import gym
+import pdb
 
 
-def welcome_main_screen():
-    """
-    Shows welcome images on the screen
-    """
+class PolicyNet(nn.Module):
+    def __init__(self):
+        super(PolicyNet, self).__init__()
 
-    p_x = int(scr_width / 5)
-    p_y = int((scr_height - game_image['player'].get_height()) / 2)
-    msgx = int((scr_width - game_image['message'].get_width()) / 2)
-    msgy = int(scr_height * 0.13)
-    b_x = 0
-    while True:
-        for event in pygame.event.get():
-            # if user clicks on cross button, close the game
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                pygame.quit()
-                sys.exit()
+        self.fc1 = nn.Linear(4, 24)
+        self.fc2 = nn.Linear(24, 36)
+        self.fc3 = nn.Linear(36, 1)  # Prob of Left
 
-            # If the user presses space or up key, start the game for them
-            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-                return
-            else:
-                display_screen_window.blit(game_image['background'], (0, 0))
-                display_screen_window.blit(game_image['player'], (p_x, p_y))
-                display_screen_window.blit(game_image['message'], (msgx, msgy))
-                display_screen_window.blit(game_image['base'], (b_x, play_ground))
-                pygame.display.update()
-                time_clock.tick(FPS)
-
-#2 be moved???
-def main_gameplay(game):
-    score = 0
-    p_x = int(scr_width / 5)
-    p_y = int(scr_width / 2)
-    b_x = 0
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        return x
 
 
-    n_pip1 = get_Random_Pipes()
-    n_pip2 = get_Random_Pipes()
+def main():
+
+    # Plot duration curve: 
+    # From http://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+    episode_durations = []
+    def plot_durations():
+        plt.figure(2)
+        plt.clf()
+        durations_t = torch.FloatTensor(episode_durations)
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(durations_t) >= 100:
+            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+        plt.pause(0.001)  # pause a bit so that plots are updated
+
+    # Parameters
+    num_episode = 5000
+    batch_size = 5
+    learning_rate = 0.01
+    gamma = 0.99
+
+    env = gym.make('CartPole-v0')
+    policy_net = PolicyNet()
+    optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=learning_rate)
+
+    # Batch History
+    state_pool = []
+    action_pool = []
+    reward_pool = []
+    steps = 0
 
 
-    up_pips = [
-        {'x': scr_width + 200, 'y': n_pip1[0]['y']},
-        {'x': scr_width + 200 + (scr_width / 2), 'y': n_pip2[0]['y']},
-    ]
+    for e in range(num_episode):
 
-    low_pips = [
-        {'x': scr_width + 200, 'y': n_pip1[1]['y']},
-        {'x': scr_width + 200 + (scr_width / 2), 'y': n_pip2[1]['y']},
-    ]
+        state = env.reset()
+        state = torch.from_numpy(state).float()
+        state = Variable(state)
+        env.render(mode='rgb_array')
 
-    pip_Vx = -4
+        for t in count():
 
-    p_vx = -9
-    p_mvx = 10
-    p_mvy = -8
-    p_accuracy = 1
+            probs = policy_net(state)
+            m = Bernoulli(probs)
+            action = m.sample()
 
-    p_flap_accuracy = -8
-    p_flap = False
+            action = action.data.numpy().astype(int)[0]
+            next_state, reward, done, _ = env.step(action)
+            env.render(mode='rgb_array')
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-                if p_y > 0:
-                    p_vx = p_flap_accuracy
-                    p_flap = True
-                    game_audio_sound['wing'].play()
+            # To mark boundarys between episodes
+            if done:
+                reward = 0
 
-        cr_tst = is_Colliding(p_x, p_y, up_pips,
-                              low_pips)
-        if cr_tst:
-            return
+            state_pool.append(state)
+            action_pool.append(float(action))
+            reward_pool.append(reward)
 
+            state = next_state
+            state = torch.from_numpy(state).float()
+            state = Variable(state)
 
-        p_middle_positions = p_x + game_image['player'].get_width() / 2
-        for pipe in up_pips:
-            pip_middle_positions = pipe['x'] + game_image['pipe'][0].get_width() / 2
-            if pip_middle_positions <= p_middle_positions < pip_middle_positions + 4:
-                score += 1
-                print(f"Your score is {score}")
-                game_audio_sound['point'].play()
+            steps += 1
 
-        if p_vx < p_mvx and not p_flap:
-            p_vx += p_accuracy
-
-        if p_flap:
-            p_flap = False
-        p_height = game_image['player'].get_height()
-        p_y = p_y + min(p_vx, play_ground - p_y - p_height)
-
-
-        for pip_upper, pip_lower in zip(up_pips, low_pips):
-            pip_upper['x'] += pip_Vx
-            pip_lower['x'] += pip_Vx
-
-
-        if 0 < up_pips[0]['x'] < 5:
-            new_pip = get_Random_Pipes()
-            up_pips.append(new_pip[0])
-            low_pips.append(new_pip[1])
-
-
-        if up_pips[0]['x'] < -game_image['pipe'][0].get_width():
-            up_pips.pop(0)
-            low_pips.pop(0)
-
-
-        display_screen_window.blit(game_image['background'], (0, 0))
-        for pip_upper, pip_lower in zip(up_pips, low_pips):
-            display_screen_window.blit(game_image['pipe'][0], (pip_upper['x'], pip_upper['y']))
-            display_screen_window.blit(game_image['pipe'][1], (pip_lower['x'], pip_lower['y']))
-
-        display_screen_window.blit(game_image['base'], (b_x, play_ground))
-        display_screen_window.blit(game_image['player'], (p_x, p_y))
-        d = [int(x) for x in list(str(score))]
-        w = 0
-        for digit in d:
-            w += game_image['numbers'][digit].get_width()
-        Xoffset = (scr_width - w) / 2
-
-        for digit in d:
-            display_screen_window.blit(game_image['numbers'][digit], (Xoffset, scr_height * 0.12))
-            Xoffset += game_image['numbers'][digit].get_width()
-        pygame.display.update()
-        time_clock.tick(FPS)
-
-
-def is_Colliding(p_x, p_y, up_pipes, low_pipes):
-    if p_y > play_ground - 25 or p_y < 0:
-        game_audio_sound['hit'].play()
-        return True
-
-    for pipe in up_pipes:
-        pip_h = game_image['pipe'][0].get_height()
-        if (p_y < pip_h + pipe['y'] and abs(p_x - pipe['x']) < game_image['pipe'][0].get_width()):
-            game_audio_sound['hit'].play()
-            return True
-
-    for pipe in low_pipes:
-        if (p_y + game_image['player'].get_height() > pipe['y']) and abs(p_x - pipe['x']) < \
-                game_image['pipe'][0].get_width():
-            game_audio_sound['hit'].play()
-            return True
-
-    return False
-
-
-def get_Random_Pipes():
-    """
-    Generate positions of two pipes(one bottom straight and one top rotated ) for blitting on the screen
-    """
-    pip_h = game_image['pipe'][0].get_height()
-    off_s = scr_height / 3
-    yes2 = off_s + random.randrange(0, int(scr_height - game_image['base'].get_height() - 1.2 * off_s))
-    pipeX = scr_width + 10
-    y1 = pip_h - yes2 + off_s
-    pipe = [
-        {'x': pipeX, 'y': -y1},  # upper Pipe
-        {'x': pipeX, 'y': yes2}  # lower Pipe
-    ]
-    return pipe
-
-if __name__ == "__main__":
-
-    isHumanPlayer = False
-    isVisual = True
-
-    pygame.init()
-    time_clock = pygame.time.Clock()
-    pygame.display.set_caption('Flappy Bird Game')
-    game_image['numbers'] = (
-        pygame.image.load('images/0.png').convert_alpha(),
-        pygame.image.load('images/1.png').convert_alpha(),
-        pygame.image.load('images/2.png').convert_alpha(),
-        pygame.image.load('images/3.png').convert_alpha(),
-        pygame.image.load('images/4.png').convert_alpha(),
-        pygame.image.load('images/5.png').convert_alpha(),
-        pygame.image.load('images/6.png').convert_alpha(),
-        pygame.image.load('images/7.png').convert_alpha(),
-        pygame.image.load('images/8.png').convert_alpha(),
-        pygame.image.load('images/9.png').convert_alpha(),
-    )
-
-    game_image['message'] = pygame.image.load('images/message.png').convert_alpha()
-    game_image['base'] = pygame.image.load('images/base.png').convert_alpha()
-    game_image['pipe'] = (pygame.transform.rotate(pygame.image.load(pipe_image).convert_alpha(), 180),
-                          pygame.image.load(pipe_image).convert_alpha()
-                          )
-
-    # Game sounds
-    game_audio_sound['die'] = pygame.mixer.Sound('sounds/die.wav')
-    game_audio_sound['hit'] = pygame.mixer.Sound('sounds/hit.wav')
-    game_audio_sound['point'] = pygame.mixer.Sound('sounds/point.wav')
-    game_audio_sound['swoosh'] = pygame.mixer.Sound('sounds/swoosh.wav')
-    game_audio_sound['wing'] = pygame.mixer.Sound('sounds/wing.wav')
-
-    game_image['background'] = pygame.image.load(bcg_image).convert()
-    game_image['player'] = pygame.image.load(player).convert_alpha()
-
-    pipe_width = game_image['pipe'][0].get_width()
-    pipe_height = game_image['pipe'][0].get_height()
-    player_width = game_image['player'].get_width()
-    player_height = game_image['player'].get_height()
-    base_height = game_image['base'].get_height()
-
-    game = environment(scr_width, scr_height,pipe_width,pipe_height,player_width,player_height,base_height)
-
-    if isHumanPlayer:
-        while True:
-            welcome_main_screen()  # Shows welcome screen to the user until he presses a button
-            main_gameplay(game)  # This is the main game function
-    
-
-    elif isVisual:
-        old_score = 0
-        cur_state,inputs = game.update(False)
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                    pygame.quit()
-                    sys.exit()
-
-            jump = badagent(cur_state)
-            if jump:
-                game_audio_sound['wing'].play()
-            if inputs[0] != old_score:
-                old_score = inputs[0]
-                print(f"Your score is {old_score}")
-                game_audio_sound['point'].play()
-
-            cur_state,inputs = game.update(jump)
-            
-            display_screen_window.blit(game_image['background'], (0, 0))
-            for pip_upper, pip_lower in zip(inputs[1], inputs[2]):
-                display_screen_window.blit(game_image['pipe'][0], (pip_upper['x'], pip_upper['y']))
-                display_screen_window.blit(game_image['pipe'][1], (pip_lower['x'], pip_lower['y']))
-
-            display_screen_window.blit(game_image['base'], (inputs[3], play_ground))
-            display_screen_window.blit(game_image['player'], (inputs[4], inputs[5]))
-            d = [int(x) for x in list(str(inputs[0]))]
-            w = 0
-            for digit in d:
-                w += game_image['numbers'][digit].get_width()
-            Xoffset = (scr_width - w) / 2
-
-            for digit in d:
-                display_screen_window.blit(game_image['numbers'][digit], (Xoffset, scr_height * 0.12))
-                Xoffset += game_image['numbers'][digit].get_width()
-
-            pygame.display.update()
-            time_clock.tick(FPS)
-
-
-    else:
-        cur_state,inputs = game.update(False)
-        i = 0
-        while True:
-            jump = badagent(cur_state)
-            cur_state,inputs = game.update(jump)
-            if i>100:
+            if done:
+                episode_durations.append(t + 1)
+                plot_durations()
                 break
-            i += 1
-            #Make main game play with read variables
+
+        # Update policy
+        if e > 0 and e % batch_size == 0:
+
+            # Discount reward
+            running_add = 0
+            for i in reversed(range(steps)):
+                if reward_pool[i] == 0:
+                    running_add = 0
+                else:
+                    running_add = running_add * gamma + reward_pool[i]
+                    reward_pool[i] = running_add
+
+            # Normalize reward
+            reward_mean = np.mean(reward_pool)
+            reward_std = np.std(reward_pool)
+            for i in range(steps):
+                reward_pool[i] = (reward_pool[i] - reward_mean) / reward_std
+
+            # Gradient Desent
+            optimizer.zero_grad()
+
+            for i in range(steps):
+                state = state_pool[i]
+                action = Variable(torch.FloatTensor([action_pool[i]]))
+                reward = reward_pool[i]
+
+                probs = policy_net(state)
+                m = Bernoulli(probs)
+                loss = -m.log_prob(action) * reward  # Negtive score function x reward
+                loss.backward()
+
+            optimizer.step()
+
+            state_pool = []
+            action_pool = []
+            reward_pool = []
+            steps = 0
+
+
+if __name__ == '__main__':
+    main()
