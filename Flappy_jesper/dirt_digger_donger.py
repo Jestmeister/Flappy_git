cef5a9efa07c494f354c8fbec61de27572c068bc
Jestmeister <63909272+Jestmeister@users.noreply.github.com>
2022-05-16 22:30:11 +0200

    cnn is trainable

--- Flappy_jesper/dirt_digger_donger.py
+++ Flappy_jesper/dirt_digger_donger.py
@@ -12,13 +12,24 @@ if go_main:
         print('')
         print('----------- NEW RUN -----------')
         print('')
-        agent = DQNagent(n_episodes=600,start_difficulty=0)
+        agent = DQNagent(n_episodes=750,start_difficulty=4)  #make 0
         agent.train()
         if agent.difficulty == 4 and agent.best_score > 5:
             print(f'Number of runs until epicness: {run_counter}')
             break   
-
-CNagent=DQNagent_cn(11,0)
-CNagent.train()
-#agent.test_scrn()
+else:
+    #CNagent=DQNagent_cn(11,0)
+    #CNagent.test_scrn()
+    #CNagent.train()
+    while True:
+        run_counter += 1
+        print('')
+        print('----------- NEW RUN -----------')
+        print('')
+        agent = DQNagent(n_episodes=500,start_difficulty=4)  #make 0
+        agent.train()
+        if agent.difficulty == 4 and agent.best_score > 5:
+            print(f'Number of runs until epicness: {run_counter}')
+            break   
+    
 
