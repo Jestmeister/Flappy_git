8619deda12b4c4a78af5b6175b8b1c7b3cd59c46
Axel Prebensen <axelprebensen@gmail.com>
2022-05-20 12:39:05 +0200

    Update main.py

--- Flappy_AxeloPhilip/main.py
+++ Flappy_AxeloPhilip/main.py
@@ -1,6 +1,6 @@
 
 import agentPG
-
+import matplotlib.pyplot as plt
 import agentManuel
 
 import agentPGCNN
@@ -9,17 +9,17 @@ import agentPGCNN
 
 def main():
     # Parameters
-    num_episode = 30000
+    num_episode = 16000
     batch_size = 1000
 
-    learning_rate = 0.001
-    learning_rate_value = 0.00005
-    gamma = 0.8
+    learning_rate = 0.01
+    learning_rate_value = 0.005
+    gamma = 0.9
 
-    start_difficulty = 4
+    start_difficulty = 0
 
-    num_pre_train = 10000
-    batch_size_after_pre_train = 400
+    num_pre_train = 4000
+    batch_size_after_pre_train = 500
     learning_rate_value_after_pre_train = 0.01 * learning_rate_value
 
     theAgent = agentPG.AgentPG()
@@ -31,14 +31,18 @@ def main():
         theAgent.preTrainValueNet = True
     else:
         theAgent.preTrainValueNet = False
-
+    plot_reward_array = []
+    plot_DiscReward_array = []
     for e in range(num_episode):
         theAgent.StartEnv()
 
         running = True
         while(running):
             running = theAgent.Update()
-
+        plot_reward_array.append(len(theAgent.reward))
+        plot_DiscReward_array.append(len(theAgent.discountedReward))
+        print(theAgent.reward)
+        # print(len(theAgent.reward))
         # Update policy
         if e > 0 and e % batch_size == 0:
             print('Batch: {}'.format(e))
@@ -51,8 +55,9 @@ def main():
                 batch_size = batch_size_after_pre_train
                 theAgent.batch_size = batch_size
                 learning_rate_value = learning_rate_value_after_pre_train
-
-
+    print(plot_reward_array)
+    plt.plot(plot_reward_array)
+    plt.show()
 
 if __name__ == '__main__':
     main()
