4a451b4c3e4f7796ad72f7a4005b5aee84cf806f
Axel Prebensen <axelprebensen@gmail.com>
2022-05-23 14:24:28 +0200

    added network

--- Flappy_AxeloPhilip/main.py
+++ Flappy_AxeloPhilip/main.py
@@ -6,17 +6,17 @@ import agentPGCNN
 
 def main():
     # Parameters
-    num_episode = 16000
-    batch_size = 800
+    num_episode = 6400
+    batch_size = 320
 
     learning_rate = 0.001
     learning_rate_value = 0.00005
     gamma = 0.9
 
-    start_difficulty = 0
+    start_difficulty = 3
 
-    num_pre_train = 4000
-    batch_size_after_pre_train = 400
+    num_pre_train = 1600
+    batch_size_after_pre_train = 160
     learning_rate_value_after_pre_train = 0.01 * learning_rate_value
 
     theAgent = agentPG.AgentPG()
@@ -39,7 +39,6 @@ def main():
             running = theAgent.Update()
         plot_reward_array.append(len(theAgent.reward))
         plot_DiscReward_array.append(len(theAgent.discountedReward))
-<<<<<<< Updated upstream
 
         if num_pre_train > e:
             theAgent.preTrainValueNet = True
@@ -48,33 +47,15 @@ def main():
             batch_size = batch_size_after_pre_train
             theAgent.batch_size = batch_size
             learning_rate_value = learning_rate_value_after_pre_train
-            
-=======
         if len(theAgent.reward) > maxscore and e > num_pre_train:
             maxscore = len(theAgent.reward)
             theAgent.savenet()
 
->>>>>>> Stashed changes
         # Update policy
         if e > 0 and e % batch_size == 0:
             print('Batch: {}'.format(e))
             theAgent.UpdatePolicy()
-<<<<<<< Updated upstream
-                
-    print(plot_reward_array)
-=======
 
-            if num_pre_train > e:
-                theAgent.preTrainValueNet = True
-            else:
-                theAgent.preTrainValueNet = False
-                batch_size = batch_size_after_pre_train
-                theAgent.batch_size = batch_size
-                learning_rate_value = learning_rate_value_after_pre_train
-            
-            theAgent.ResetParm()
-
->>>>>>> Stashed changes
     plt.plot(plot_reward_array)
     plt.show()
 
